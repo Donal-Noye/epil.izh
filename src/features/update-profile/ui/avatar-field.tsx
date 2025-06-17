c77a849f0e9f2b486b5d0ace2c2234501777b649
donalnoye <leekeiz49@gmail.com>
2025-06-17 23:16:25 +0400

    feat: add upload file action

--- src/features/update-profile/ui/avatar-field.tsx
+++ src/features/update-profile/ui/avatar-field.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/shared/ui/button";
 // import { Spinner } from "@/shared/ui/spinner";
 import { ProfileAvatar } from "@/services/user/ui/profile-avatar";
+import { useUploadAvatar } from "@/features/update-profile/vm/use-upload-avatar";
+import { Spinner } from "@/shared/ui/spinner";
 
 export function AvatarField({
   value,
@@ -9,19 +11,21 @@ export function AvatarField({
   value?: string;
   // onChange: (value?: string) => void;
 }) {
+  const {handleSelectFile, isPending} = useUploadAvatar({})
+
   return (
     <Button
       variant="ghost"
       className="w-[84px] h-[84px] p-0.5 rounded-full relative block"
-      disabled={false}
-      onClick={() => {}}
+      disabled={isPending}
+      onClick={handleSelectFile}
       type="button"
     >
-      {/*{false && (*/}
-      {/*  <div className="inset-0 absolute flex items-center justify-center z-10">*/}
-      {/*    <Spinner className="w-10 h-10" aria-label="Загрузка новой аватарки" />*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      {isPending && (
+        <div className="inset-0 absolute flex items-center justify-center z-10">
+          <Spinner className="w-10 h-10" aria-label="Загрузка новой аватарки" />
+        </div>
+      )}
       <ProfileAvatar
         className="w-full h-full"
         profile={{ email: "leekeiz49@gmal.com", image: value }}
