9d56c5debf5ea20786cd528b03654b96a51eef1b
donalnoye <leekeiz49@gmail.com>
2025-06-30 23:44:44 +0400

    feat: add viewport height

--- src/shared/lib/react.tsx
+++ src/shared/lib/react.tsx
@@ -88,3 +88,19 @@ export function useEventCallback<A extends unknown[], R>(fn: Fn<A, R>): Fn<A, R>
     [],
   );
 }
+
+export function useViewportHeight() {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener("resize", setVh);
+
+    return () => {
+      window.removeEventListener("resize", setVh);
+    };
+  }, []);
+}
