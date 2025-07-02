dcd1ca282f8821ce0830a832ca030c09c995c08b
donalnoye <leekeiz49@gmail.com>
2025-07-02 20:37:52 +0400

    chor: add get all users query

--- src/entities/user/queries/index.ts
+++ src/entities/user/queries/index.ts
@@ -1,12 +1,18 @@
 import { UserId } from "@/kernel/domain/user";
 import { getUserProfileAction } from "@/entities/user/actions/get-user-profile";
 import { useQueryClient } from "@tanstack/react-query";
+import { getAllUsersAction } from "@/entities/user/actions/get-all-users";
 
 const baseKey = "user";
 
 export const getProfileQuery = (userId: UserId) => ({
   queryKey: [baseKey, "getProfileById", userId],
   queryFn: () => getUserProfileAction({ userId }),
+});
+
+export const getAllUsersQuery = () => ({
+  queryKey: [baseKey, "getAllUsers"],
+  queryFn: getAllUsersAction,
 });
 
 export const useInvalidateProfile = () => {
